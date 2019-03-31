eb2c72aaaef21a3bc02c85dde536c4fdac800b5a
BigGlebovsky <45733239+BigGlebovsky@users.noreply.github.com>
2019-03-31 16:02:59 +0300

    test
    test

--- cont.js
+++ cont.js
@@ -16,7 +16,9 @@ function controllerGame() {
         myField.addEventListener("touchstart", myModel.touchStart);
     
         myField.addEventListener("touchmove", myModel.ftouchMove);
-    
+        
+        myField.addEventListener("touchstart", myModel.begin);
+        
         myField.addEventListener("touchend", myModel.touchEnd);    
     
         myField.addEventListener("click", myModel.begin);
